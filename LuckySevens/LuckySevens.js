76a01e5472d3cdd95c88cec9a220e696b819423c
James McCracken <mccrackenljames@gmail.com>
2018-03-18 22:37:16 -0500

    Dev10Project
    Project Files for Dev10 Software Guild Projects.

---
+++ LuckySevens/LuckySevens.js
@@ -0,0 +1,43 @@
+
+function hideResults() {
+document.getElementById("results").style.display = "none";
+}
+<!-- Need warning message to stop bets under $1 or make it a bet entry form? -->
+
+function play() {
+    var startingBet = document.getElementById("betInput").value;
+    var bet = startingBet;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+    var diceRoll = dice1 + dice2;
+    var betsArray = [];
+
+    while (bet > 0) {
+        if(diceRoll != 7) {
+            bet -= 1
+        } else {
+            bet += 4
+        }
+        betsArray.push(bet)
+      /*  <!-- Repeat diceRoll Loop Function. Keep rolling until broke --> */
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        var diceRoll = dice1 + dice2;
+    }
+
+    var rollCounter = betsArray.length;
+    var highestAmount = Math.max.apply(Math, betsArray);
+    var highestPosition = betsArray.indexOf(highestAmount);
+    var rollsFromHighest = rollCounter - highestPosition;
+
+    function showResults() {
+    document.getElementById("results").style.display = "inline";
+    document.getElementById("playButton").innerHTML = "Play Again ?";
+    document.getElementById("resultsBet").innerHTML = "$" + startingBet +".00";
+    document.getElementById("resultsRollCounter").innerHTML = rollCounter;
+    document.getElementById("resultsHighestHeld").innerHTML = "$" + highestAmount + ".00";
+    document.getElementById("resultsRollsFromHighest").innerHTML = rollsFromHighest;
+    };
+
+    showResults();
+}
